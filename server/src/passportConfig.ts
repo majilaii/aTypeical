70ccdc33f8e258a917f809ef1d6894735f7866bc
Isaac Pillar <isaac.pillar@icloud.com>
2022-11-22 13:15:04 +0000

    refactor(passportconfig.ts): create user interface and correctly type err using CallbackError

--- server/src/passportConfig.ts
+++ server/src/passportConfig.ts
@@ -2,12 +2,33 @@ import passport, { PassportStatic } from "passport";
 
 import User from './model/users';
 import bcrypt from 'bcrypt';
+import { CallbackError, Document } from "mongoose";
 const localStrategy = require('passport-local').Strategy;
+
+interface user extends Document {
+  created: Date;
+  username: string;
+  email: string;
+  password: string;
+  history: {
+      date?: Date;
+      wpm?: number;
+      accuracy?: number;
+      rawwpm?: number;
+      time?: number;
+        textLength?: number;
+        incorrect?: number;
+        wordAmount?: number;
+        KEnglish?: number;
+        typingMode?: number;
+    }[];
+}
+
 
 module.exports = function (passport: PassportStatic) {
   passport.use(
     new localStrategy((username: string, password: string, done: Function) => {
-      User.findOne({ username: username }, (err: any, user: any) => {
+      User.findOne({ username: username }, (err: CallbackError, user: user) => {
         if (err) throw err;
         if (!user) return done(null, false);
         bcrypt.compare(password, user.password, (err, result) => {
@@ -22,12 +43,12 @@ module.exports = function (passport: PassportStatic) {
     })
   );
 
-  passport.serializeUser((user: any, cb) => {
-    cb(null, user.id);
+  passport.serializeUser((user, cb) => {
+    cb(null, (user as {id:number}).id);
   });
 
   passport.deserializeUser((id, cb) => {
-    User.findOne({ _id: id }, (err: any, user: any) => {
+    User.findOne({ _id: id }, (err: CallbackError, user: user) => {
       cb(err, user);
     });
   });
