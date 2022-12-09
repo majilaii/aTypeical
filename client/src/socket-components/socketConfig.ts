6efd5f9f94275eabe5e8460e5f773bddef0ab911
Isaac Pillar <isaac.pillar@icloud.com>
2022-12-09 18:39:43 +0000

    refactor: test

--- client/src/socket-components/socketConfig.ts
+++ client/src/socket-components/socketConfig.ts
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client';
-const socket = io(process.env.SERVER || 'http://localhost:4000');
+const socket = io(process.env.SERVER || 'asdfasdfasdfhttp://localhost:4000');
 
 socket.on('message', (msg: string) => console.log(msg));
 
